5bf82296581406af0b8f49e3a7714d9bea393471
uriel <uriel781@gmail.com>
2025-07-29 12:51:55 -0500

    Fix duplicate factory definition by consolidating user and event_log factories

--- factories/users.rb
+++ factories/users.rb
@@ -2,9 +2,26 @@
 
 FactoryBot.define do
   factory :user do
-    email {  "test@example" }
-    password { "password" }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+    name { Faker::Name.name }
     role { :user }
     subscription_type { :free }
+
+    trait :admin do
+      role { :admin }
+    end
+
+    trait :premium do
+      subscription_type { :premium }
+    end
+  end
+
+  factory :event_log do
+    association :user
+    event_type { EventLog::EVENT_TYPES['Page View'] }
+    metadata { { page_path: '/dashboard', referrer: 'google.com' } }
+    occurred_at { Time.current }
   end
 end
