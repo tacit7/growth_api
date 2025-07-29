1655b7328993e88ce85afb7f91249afa2d7a30b7
uriel <uriel781@gmail.com>
2025-07-29 12:47:22 -0500

    Fix EventLoggerJob to use proper data structure and model associations

--- app/jobs/event_logger_job.rb
+++ app/jobs/event_logger_job.rb
@@ -4,13 +4,17 @@ class EventLoggerJob < ApplicationJob
   queue_as :events
 
   def perform(args)
+    user = User.find(args[:user_id])
+    event_type_id = EventLog::EVENT_TYPES[args[:event_type]] || args[:event_type]
+    
     EventLog.create!(
-      user_id:     args[:user_id],
-      event_type:  args[:event_type],
-      event_data:  args.fetch(:event_data, {}),
-      occurred_at: Time.iso8601(args[:occurred_at]),
-      ip_address:  args[:ip_address],
-      user_agent:  args[:user_agent]
+      user: user,
+      event_type: event_type_id,
+      metadata: args.fetch(:event_data, {}).merge(
+        ip_address: args[:ip_address],
+        user_agent: args[:user_agent]
+      ),
+      occurred_at: Time.iso8601(args[:occurred_at])
     )
   end
 
